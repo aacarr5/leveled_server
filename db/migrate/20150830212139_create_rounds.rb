fdf118713e83c1aa462a48740ede01de6278e61e
Andrew Carr <aacarr5@gmail.com>
2015-09-02 14:01:00 -0500

    Remove point column

--- db/migrate/20150830212139_create_rounds.rb
+++ db/migrate/20150830212139_create_rounds.rb
@@ -4,7 +4,6 @@ class CreateRounds < ActiveRecord::Migration
     	t.integer :completed_time
     	t.integer :intended_time
     	t.integer :user_id
-    	t.integer :points
 
     	t.timestamp null:false
     end
