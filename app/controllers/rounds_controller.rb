0f815175f7504a8bb5f6d09d2ad8316f68c3d512
Andrew Carr <aacarr5@gmail.com>
2015-09-02 13:59:22 -0500

    Fix indentation

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,10 +1,13 @@
 class RoundsController < ApplicationController
 
 	def create
+		puts params
 		respond_to do |format|
 			format.html {render :show}
 			format.json do round = Round.create!(round_params)
-						   render json: {"user" => round.points}
+							# user = User.find(round.user_id)
+							# user.add_points(round.points)
+						   render json: {"response" => round.points}
 						end
 					end
 	end
