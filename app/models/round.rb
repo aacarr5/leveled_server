d4240cd7c3bf8b98cf8929c53e736779133d864f
Andrew Carr <aacarr5@gmail.com>
2015-09-02 17:22:12 -0500

    Update algorithm

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,7 +2,7 @@ class Round < ActiveRecord::Base
 	belongs_to :user
 
 	def points
-		base_points = (self.intended_time - self.completed_time / 60).to_i
+		base_points = ((self.completed_time) / 60).to_i
 		base_points += 10 if self.intended_time == self.completed_time
 		base_points
 	end
