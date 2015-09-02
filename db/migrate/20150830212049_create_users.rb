57e62db1b501e19c7f681abdefe44574c6501a7a
Andrew Carr <aacarr5@gmail.com>
2015-09-01 21:56:45 -0500

    Create default values

--- db/migrate/20150830212049_create_users.rb
+++ db/migrate/20150830212049_create_users.rb
@@ -3,8 +3,8 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
     	t.string :username
     	t.string :password
-    	t.integer :level
-    	t.integer :xp
+    	t.integer :level, default: 1
+    	t.integer :xp, default: 1
 
     	t.timestamp null:false
     end
