8b6428a693a46ba32eff572a9769197e661d3f25
Andrew Carr <aacarr5@gmail.com>
2015-09-02 14:00:25 -0500

    Update point method

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,13 +2,13 @@ class Round < ActiveRecord::Base
 	belongs_to :user
 
 	def points
-		@points || = pointCreator
+		base_points = self.completed_time / self.intended_time
+		base_points += 10 if base_points == 1
+		base_points += 10 #bc we're fun like that
+		base_points
 	end
 
+	
 
-	private
-		def pointCreator
-			base_points = self.completed_time / self.inteded_time
-			base_points += 10 if base_points == 1
-		end
+
 end
