afaabe708d7449159841dd74b8263bd11537affb
Andrew Carr <aacarr5@gmail.com>
2015-09-02 17:09:08 -0500

    Fix typo

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,9 +2,8 @@ class Round < ActiveRecord::Base
 	belongs_to :user
 
 	def points
-		base_points = self.completed_time / self.intended_time
-		base_points += 10 if base_points == 1
-		base_points += 10 #bc we're fun like that
+		base_points = (self.intended_time - self.completed_time / 60).to_i
+		base_points += 10 if self.intended_time == self.completed_time
 		base_points
 	end
 
